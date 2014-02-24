5a8c445dd1f6a416db7e6ee48fbd2d4dcf0773e0
Maja Frydrychowicz <mjzffr@gmail.com>
2014-02-24 14:37:49 -0500

    Cleaned up server code. Added start_client() function

--- network/simpleChatServers.py
+++ network/simpleChatServers.py
@@ -10,9 +10,13 @@ and the server will echo what client k sends to all clients.
 (A chat server!)
 (learning goal: reimplement what tom demo'ed the other day)
 
-Note (Check this):  a.send(m) := send m TO a
+Note:  a.send(m) := send m TO a
                     a.recv(x) := receive  <= x bytes FROM a
 '''
+
+HOST = 'localhost'
+PORT = 50000
+SIZE = 1024
 
 def bind_listening_socket():
     # default params are INET and STREAM
@@ -20,7 +24,7 @@ def bind_listening_socket():
     #prevent the "already in use error"
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     # TODO: bind raises an exception if 8000 is already in use
-    s.bind(('localhost', 8000))
+    s.bind((HOST, PORT))
     s.listen(5)
 
     return s
@@ -55,7 +59,7 @@ def start_terrible_server():
         for cur_client in clients:
             msg = ''
             try:
-                msg = cur_client.recv(1024)
+                msg = cur_client.recv(SIZE)
             except socket.error, e:
                 # same idea as exception above: this is the point
                 # of this terrible server
@@ -66,24 +70,26 @@ def start_terrible_server():
                     if otherclient is not cur_client:
                         otherclient.send(msg) 
 
-    # TODO: what about closing the connections? Important?
+    # TODO: what about closing the connections? Important? A: Server
+    # should close its listening connection.
 
 def start_nicer_server():
     ''' Uses select to only handle sockets that are ready/available.'''
-    
+    print "Server on"
     listener = bind_listening_socket()
 
-    # listening socket should be examined by select too so that it doesn't
-    # block on "accept"
-    # Including stdin to allow 'exit' command on server side
+    '''listening socket should be examined by select too so that it doesn't
+        block on "accept".
+        Including stdin to allow 'exit' command on server side
+        (readline() is also blocking) '''
     all_input = [listener,sys.stdin]
     all_clients = []
 
     while True:
-        # pull out the sockets that are ready to be read from
-        # i.e. ready to recv/accept?
-        # aside/experiment: time-out 0 (i.e. select(x,x,x,0)) used 
-        #   a lot of CPU resources 
+        ''' select the sockets that are ready for blocking calls
+            like recv and accept
+        aside/experiment: time-out 0 (i.e. select(x,x,x,0)) used 
+        a lot of CPU resources''' 
         ready_input, _, _ = select.select(all_input, [], [])
 
         for cur_input in ready_input:
@@ -95,14 +101,15 @@ def start_nicer_server():
                 clientsocket.send("Hello to " + str(address) + '\n')
             elif cur_input == sys.stdin:
                 if (sys.stdin.readline()).strip() == 'exit':
-                    # TODO: important to close all the client sockets, too?
+                    # (?): assume that client is responsible for closing
+                    # its connection
                     listener.close()
+                    print "Server off"
                     sys.exit()
             # collect a message from each ready client
-            # ask about this (excluding listener)
             # Maybe better to just move the listener to different select?
             else:
-                msg = cur_input.recv(1024)
+                msg = cur_input.recv(SIZE)
                 if msg:
                     # and send the message to everyone
                     # (check allInput not readyInput because we're sending
@@ -111,5 +118,43 @@ def start_nicer_server():
                         if otherinput is not cur_input:
                             otherinput.send(msg) 
 
+def start_client():
+    s = socket.socket()
+    s.connect((HOST,PORT))
+    # A port is assigned to s after the above connection is established
+    (s_ip, s_port) = s.getsockname()
+    all_input = [s, sys.stdin]
+
+    while True:
+        ready_input, _, _ = select.select(all_input, [], [])
+        for input in ready_input:
+            if input is sys.stdin:
+                line = sys.stdin.readline()
+                if line.strip() == 'exit':
+                    s.send(str(s_port) + " left.\n")
+                    s.close()
+                    sys.exit()
+                s.send(str(s_port) + ": " + line)
+                display_prompt()
+            if input is s:
+                data = input.recv(SIZE)
+                if len(data) == 0:
+                    print '\nConnection lost'
+                    s.close()
+                    sys.exit()
+                else:
+                    sys.stdout.write('\n' + data)
+                    display_prompt()
+    
+def display_prompt():
+    sys.stdout.write('>>')
+    ''' sys.stdout.write() isn't guaranteed to display text 
+    immediately, so flush. Same problem with print''' 
+    sys.stdout.flush()
+
+
 if __name__ == "__main__":
-    start_nicer_server()
+    if len(sys.argv) == 2 and sys.argv[1] == 'client':
+        start_client()
+    else:
+        start_nicer_server()
